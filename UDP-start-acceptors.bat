b1405ee4ec91caa7a925c3d83456f47ceb1cab38
gili-julio <giliardoplay@gmail.com>
2026-04-22 16:53:45 -0300

    adiciona scripts para inicialização das instancias em UDP e TCP

---
+++ UDP-start-acceptors.bat
@@ -0,0 +1,5 @@
+@echo off
+set ROOT=%~dp0
+start "Acceptor :10001" cmd /k "java -jar "%ROOT%paxos-acceptor\target\paxos-acceptor-1.0-SNAPSHOT-jar-with-dependencies.jar" --protocol=udp --port=10001 --gateway=localhost:8080"
+start "Acceptor :10002" cmd /k "java -jar "%ROOT%paxos-acceptor\target\paxos-acceptor-1.0-SNAPSHOT-jar-with-dependencies.jar" --protocol=udp --port=10002 --gateway=localhost:8080"
+start "Acceptor :10003" cmd /k "java -jar "%ROOT%paxos-acceptor\target\paxos-acceptor-1.0-SNAPSHOT-jar-with-dependencies.jar" --protocol=udp --port=10003 --gateway=localhost:8080"
